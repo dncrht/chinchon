4e356de34cd253e9217a7bb6c0a201e79bc6b621
Dani <daniel.cr.ht@gmail.com>
2016-08-11 19:24:01 +0200

    Cordoba browser configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'public');
+var BUILD_DIR = path.resolve(__dirname, 'www');
 var APP_DIR = path.resolve(__dirname, 'app');
 
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
