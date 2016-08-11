8f51743cc3768257280bb81d2af9046362e9bc8d
Dani <daniel.cr.ht@gmail.com>
2016-08-11 19:00:08 +0200

    Added Bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,12 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'app');
 
-var config = {
-  entry: APP_DIR + '/index.jsx',
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = [{
+  entry: [
+    APP_DIR + '/index.jsx',
+  ],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
@@ -19,6 +23,30 @@ var config = {
       }
     ]
   }
-};
-
-module.exports = config;
+},
+{
+  entry: [
+    APP_DIR + '/styles/application.sass',
+  ],
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.css'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg)$/,
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]'
+      },
+      {
+        test: /\.sass$/,
+        loader: ExtractTextPlugin.extract('css!sass')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css', {
+      allChunks: true
+    })
+  ]
+}];
